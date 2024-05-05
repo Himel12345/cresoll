4fe3083dc994d081a98187a9409b3145267a8e61
Himel <shariyerhimel541@gmail.com>
2024-05-05 16:55:40 +0600

    hero section added

--- src/pages/Shared/Banner/Banner.jsx
+++ src/pages/Shared/Banner/Banner.jsx
@@ -4,9 +4,8 @@ import bannerimg from "../../../assets/banner/bannerimg.jpg"
 const Banner = () => {
     return (
         <div className='bg'>
-            <h1>CHANGE <br></br><span className='look'>THE LOOK</span> </h1>
+            <h1 className='change'>CHANGE <br></br><span className='look'>THE LOOK</span> </h1>
             <div className='banner-img'>
-              
                 <img src={bannerimg} alt="" />
                 </div>
         </div>
